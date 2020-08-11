2a1e06f40d365d448682622ed752cbc0eb773d50
Tucker Barrett <tuckerbarrett@gmail.com>
2020-08-11 16:38:16 -0400

    Edit two exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatTimes) {
+    if (repeatTimes < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    for (let i = 0; i < repeatTimes; i++) {
+        newString += word;
+    }
+    return newString;
 }
 
 module.exports = repeatString
